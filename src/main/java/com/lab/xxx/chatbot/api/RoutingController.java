03afdbcf45d745cb2840bf488f06bb283344dbe1
Rene Hengstermann <rhengstermann@d8bc8436.prodyna.com>
2018-02-01 16:28:28 +0000

    Added output for APIv2 request

--- src/main/java/com/lab/xxx/chatbot/api/RoutingController.java
+++ src/main/java/com/lab/xxx/chatbot/api/RoutingController.java
@@ -4,6 +4,7 @@ import com.google.cloud.dialogflow.v2beta1.EntityTypesClient;
 import com.google.cloud.dialogflow.v2beta1.Intent;
 import com.google.cloud.dialogflow.v2beta1.WebhookRequest;
 import com.google.cloud.dialogflow.v2beta1.WebhookResponse;
+import com.google.gson.Gson;
 import com.lab.xxx.chatbot.api.model.DFRequestBody;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -22,23 +23,12 @@ public class RoutingController {
 
     }
 
-    @RequestMapping(value = "/query", method = RequestMethod.POST)
+    @RequestMapping(value = "/api", method = RequestMethod.POST)
     public String executeAction(@RequestBody String body) {
         System.out.println("custom intent called.");
         System.out.println("Recived body: " + body);
 
-        return "Ihre Anfrage war erfolgreich.";
-
+        return WebhookResponse.newBuilder().setFulfillmentText("This is a Service response").build().toString();
     }
 
-
-    @RequestMapping(value = "/api", method = RequestMethod.POST)
-    public WebhookResponse postApi(@ModelAttribute WebhookRequest request){
-
-        System.out.println("V2 API Intend called! action:" + request.getQueryResult().getAction());
-        return WebhookResponse.newBuilder().addFulfillmentMessages(
-                Intent.Message.newBuilder().setText(
-                Intent.Message.Text.newBuilder().addText("MAd MAx 93").build()
-        ).build()).build();
-    }
 }
