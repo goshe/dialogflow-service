ecefeb7936e7103203eb05341dc541805f04003c
Rene Hengstermann <rhengstermann@d8bc8436.prodyna.com>
2018-02-01 17:17:57 +0000

    Added parsing of request body

--- src/main/java/com/lab/xxx/chatbot/api/RoutingController.java
+++ src/main/java/com/lab/xxx/chatbot/api/RoutingController.java
@@ -1,13 +1,10 @@
 package com.lab.xxx.chatbot.api;
 
-import com.google.cloud.dialogflow.v2beta1.EntityTypesClient;
-import com.google.cloud.dialogflow.v2beta1.Intent;
-import com.google.cloud.dialogflow.v2beta1.WebhookRequest;
-import com.google.cloud.dialogflow.v2beta1.WebhookResponse;
 import com.google.gson.Gson;
-import com.lab.xxx.chatbot.api.model.DFRequestBody;
-import org.springframework.http.MediaType;
+import com.google.gson.GsonBuilder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Properties;
 
 /**
  * Controller which routes requests for the Zeppelin Chatbot API.
@@ -16,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class RoutingController {
+    private static final Gson GSON = new GsonBuilder().create();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
@@ -28,14 +26,22 @@ public class RoutingController {
     public String executeAction(@RequestBody String body) {
         System.out.println("custom intent called.");
         System.out.println("Recived body: " + body);
-        String response = "{\"fulfillmentText\": \"Ja, wir haben am 2018-02-05/2018-02-11 ein 906M für den Einsatz in 63225 verfügbar.\",\n" +
-                "     \"fulfillmentMessages\": [{\n" +
-                "       \"text\": {\n" +
-                "         \"text\": [\"Ja, wir haben am 2018-02-05/2018-02-11 ein 906M für den Einsatz in 63225 verfügbar.\"]\n" +
-                "       }\n" +
-                "}]}";
+        Properties data = GSON.fromJson(body, Properties.class);
+
+        data.list(System.out);
+
+        String response = createJSONResponse("Response from Service !");
         System.out.println("Will send response " + response);
         return response;
+    }
+
+    private String createJSONResponse(String messageText) {
+        return "{\"fulfillmentText\": \""+messageText+"\",\n" +
+                "     \"fulfillmentMessages\": [{\n" +
+                "       \"text\": {\n" +
+                "         \"text\": [\""+messageText+"\"]\n" +
+                "       }\n" +
+                "}]}";
     }
 
 }
